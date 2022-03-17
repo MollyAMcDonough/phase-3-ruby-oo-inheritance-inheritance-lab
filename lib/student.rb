db8f6da89c67980f4ce5fb3ab863abd62ed45cd5
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-16 22:53:09 -0400

    commit tree 25253af023f5f1dd7d9624f384a4d618ab3d2af4

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require './lib/user.rb'
+require 'pry'
+
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn (info)
+        @knowledge.push(info)
+    end
+
 
 end
+
+molly = Student.new
+#binding.pry
+0
